eb2159516b4adae51fac4fe3c3fe78e5818d738c
Helium-Studio <xyc1123@outlook.com>
2024-01-31 17:06:47 +0800

    zram: Hardcode to 8G
    * As per some users' advise

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1742,7 +1742,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = (u64)6144 * SZ_1M;
+	disksize = (u64)8192 * SZ_1M;
 	if (!disksize)
 		return -EINVAL;
 
